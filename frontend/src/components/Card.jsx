67737da700ccfde4fc53be681d147c7463dacbcd
Abdullah bhatti <bhatti253r@gmail.com>
2025-07-14 16:08:53 +0500

    upload card comp

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 function Card() {
   const pods = ['pods1','pods2','pods3','pods4']
   return (
-    <div>
+    <div className='flex gap-4 flex-wrap justify-center'>
       {
         pods.map((pod,index)=>
-          <img src={`./img/${pod}.webp`} key={index} alt={`${pod}`} />
+          <ProductCard
+            path={`./img/${pod}.webp`}
+            key={index} 
+            alt={`${pod}`}
+          />
         )
       }
     </div>
