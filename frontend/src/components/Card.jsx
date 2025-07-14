5bd8d18dd8abb405598811a5350751a2db7a0834
Munaeem <munaeem272@gmail.com>
2025-07-14 19:25:59 +0500

    scroll complete

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 
-const names = ["Vespor Airpuds", "Eclipse Earbuds", "Nova Buds", "Galaxy Pods","Eclipse Earbuds"];
+const names = ["Vespor Airpuds", "Eclipse Earbuds", "Nova Buds", "Galaxy Pods", "Eclipse Earbuds"];
 const description = [
   "Experience the future of sound with Vespor Airpuds.",
   "Eclipse Earbuds: Where Style Meets Sound.",
@@ -9,22 +9,23 @@ const description = [
   "Galaxy Pods: Explore the Universe of Sound.",
   "Eclipse Earbuds: Where Style Meets Sound."
 ];
+
 function Card() {
-  const pods = ["pods1", "pods2", "pods3", "pods4","pods2"];
+  const pods = ["pods1", "pods2", "pods3", "pods4", "pods2"];
 
   return (
-   <div className="flex gap-5 flex-nowrap overflow-x-auto sm:overflow-x-hidden px-4 my-4 scroll-smooth">
-
-
-      {pods.map((pod, index) => (
-        <ProductCard
-          path={`./img/${pod}.webp`}
-          key={index}
-          alt={`${pod}`}
-          name={names[index]}
-          desc = {description[index]}
-        />
-      ))}
+    <div className="w-full overflow-x-auto sm:overflow-x-visible">
+      <div className="flex flex-nowrap sm:flex-wrap gap-5 px-4 my-4 scroll-smooth sm:justify-center">
+        {pods.map((pod, index) => (
+          <ProductCard
+            key={index}
+            path={`./img/${pod}.webp`}
+            alt={`${pod}`}
+            name={names[index]}
+            desc={description[index]}
+          />
+        ))}
+      </div>
     </div>
   );
 }
